5849b16f66456530391b2b506c1c50e64b738778
Chris Clayson <chris@clayson.org.uk>
2020-09-19 23:44:24 +0100

    WEB-6: Add CKEditor Bundle
    Follow instructions from here to add CKEditor https://symfony.com/doc/current/bundles/FOSCKEditorBundle/installation.html
    Did not configure Twig until implications have been evaluated

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
