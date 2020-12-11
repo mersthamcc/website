c7c735f862db85f190fcabc8ee4fd060be6a2f04
Chris Clayson <chris@clayson.org.uk>
2020-12-11 23:50:06 +0000

    WEB-17: Unit tests for Required Action Factory

--- keycloak/sms-2fa/src/test/java/uk/co/mersthamcc/keycloak/actions/MccOtpConfigureSmsActionFactoryTest.java
+++ keycloak/sms-2fa/src/test/java/uk/co/mersthamcc/keycloak/actions/MccOtpConfigureSmsActionFactoryTest.java
@@ -1,20 +1,32 @@
 package uk.co.mersthamcc.keycloak.actions;
 
 import org.junit.jupiter.api.Test;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.models.KeycloakSession;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.mockito.Mockito.mock;
 
 class MccOtpConfigureSmsActionFactoryTest {
 
+    private final MccOtpConfigureSmsActionFactory factory = new MccOtpConfigureSmsActionFactory();
+
     @Test
     void create() {
+        RequiredActionProvider action = factory.create(mock(KeycloakSession.class));
+
+        assertThat(action, instanceOf(MccOtpConfigureSmsAction.class));
     }
 
     @Test
     void getId() {
+        assertThat(factory.getId(), equalTo(MccOtpConfigureSmsAction.PROVIDER_ID));
     }
 
     @Test
     void getDisplayText() {
+        assertThat(factory.getDisplayText(), equalTo("Configure SMS for OTP"));
     }
 }
