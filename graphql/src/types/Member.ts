89c6a354340f95b8455a484582af671c0b4e192a
Chris Clayson <chris@clayson.org.uk>
2021-01-12 20:04:09 +0000

    WEB-6: Membership Database Migrations and Prisma Types

--- graphql/src/types/Member.ts
+++ graphql/src/types/Member.ts
@@ -4,5 +4,30 @@ export const Member = objectType({
     name: "Member",
     definition(t) {
         t.model.id();
+        t.model.givenName();
+        t.model.familyName();
+        t.model.gender();
+        t.model.dob();
+        t.model.registrationDate();
+        t.model.attributes();
     },
 });
+
+export const MemberAttribute = objectType({
+    name: "MemberAttribute",
+    definition(t) {
+        t.model.id();
+        t.model.createdDate();
+        t.model.value();
+        t.model.definition();
+    },
+});
+
+export const AttributeDefinition = objectType({
+    name: "AttributeDefinition",
+    definition(t) {
+        t.model.id();
+        t.model.key();
+        t.model.type();
+    }
+})
--- sql/migrations/V2__membership_tables.sql
+++ sql/migrations/V2__membership_tables.sql
@@ -1,10 +1,48 @@
+CREATE TYPE attribute_type AS ENUM ('String', 'Number', 'Boolean', 'Date', 'Time', 'Timestamp');
+CREATE TABLE attribute_definition
+(
+    id      SERIAL,
+    key     VARCHAR(64) NOT NULL,
+    type    attribute_type  NOT NULL,
+    encrypt BOOLEAN DEFAULT FALSE,
+    choices JSONB,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE member_category
+(
+    id SERIAL,
+    key VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE member
 (
     id                SERIAL,
-    given_name        VARCHAR(64),
-    family_name       VARCHAR(64),
-    gender            VARCHAR(1),
+    given_name        VARCHAR(64) NOT NULL,
+    family_name       VARCHAR(64) NOT NULL,
+    gender            VARCHAR(1) NOT NULL,
     dob               DATE,
-    registration_date TIMESTAMP,
+    registration_date TIMESTAMP NOT NULL,
+    category_id       BIGINT NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE INDEX IDX_MEMBER_CATEGORY_ID ON member (category_id);
+ALTER TABLE member
+    ADD CONSTRAINT FK_MEMBER_MEMBER_CATEGORY_ID FOREIGN KEY (category_id) REFERENCES member_category (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE TABLE member_attribute
+(
+    id              SERIAL,
+    created_date    TIMESTAMP NOT NULL,
+    attribute_id    BIGINT NOT NULL,
+    member_id       BIGINT NOT NULL,
+    value           JSONB,
+    PRIMARY KEY (id)
+);
+CREATE INDEX IDX_MEMBER_ATTRIBUTE_ATTRIBUTE_ID ON member_attribute (attribute_id);
+ALTER TABLE member_attribute
+    ADD CONSTRAINT FK_MEMBER_ATTRIBUTE_ATTRIBUTE_ID FOREIGN KEY (attribute_id) REFERENCES attribute_definition (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
+CREATE INDEX IDX_MEMBER_ATTRIBUTE_MEMBER_ID ON member_attribute (member_id);
+ALTER TABLE member_attribute
+    ADD CONSTRAINT FK_MEMBER_ATTRIBUTE_MEMBER_ID FOREIGN KEY (member_id) REFERENCES member (id) NOT DEFERRABLE INITIALLY IMMEDIATE;
