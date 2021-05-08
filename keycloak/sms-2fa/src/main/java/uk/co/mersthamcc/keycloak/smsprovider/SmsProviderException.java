d4999a65489fb1942134c1c108fde878a40c87bf
Chris Clayson <chris@clayson.org.uk>
2021-05-08 15:26:21 +0100

    WEB-38: Move Keycloak and GraphQL to their own respositories

--- keycloak/sms-2fa/src/main/java/uk/co/mersthamcc/keycloak/smsprovider/SmsProviderException.java
+++
@@ -1,7 +0,0 @@
-package uk.co.mersthamcc.keycloak.smsprovider;
-
-public class SmsProviderException extends RuntimeException {
-    public SmsProviderException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
