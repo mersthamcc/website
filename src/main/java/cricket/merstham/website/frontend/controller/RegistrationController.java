5b5785f4dacb25d0f37dabe5943bfb2978a05f5d
Chris Clayson <chris@clayson.org.uk>
2021-05-12 00:49:02 +0100

    WEB-39: Implement sessions with Redis

--- src/main/java/cricket/merstham/website/frontend/controller/RegistrationController.java
+++ src/main/java/cricket/merstham/website/frontend/controller/RegistrationController.java
@@ -1,22 +1,29 @@
 package cricket.merstham.website.frontend.controller;
 
 import com.apollographql.apollo.api.Response;
+import cricket.merstham.website.frontend.model.RegistrationBasket;
 import cricket.merstham.website.frontend.service.GraphService;
 import cricket.merstham.website.graph.MembershipCategoriesQuery;
 import cricket.merstham.website.graph.type.StringFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
-import java.security.Principal;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("basket")
+@PreAuthorize("isAuthenticated()")
 public class RegistrationController {
+    private static final Logger LOG = LoggerFactory.getLogger(RegistrationController.class);
 
     private GraphService membershipCategoryService;
 
@@ -25,21 +32,23 @@ public class RegistrationController {
         this.membershipCategoryService = membershipCategoryService;
     }
 
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(value = "/register", name = "register", method = RequestMethod.GET)
-    public ModelAndView register(Principal principal) throws IOException {
-        MembershipCategoriesQuery query = new MembershipCategoriesQuery(StringFilter.builder().build());
-        Response<MembershipCategoriesQuery.Data> result = membershipCategoryService.executeQuery(query);
+    @ModelAttribute("basket")
+    public RegistrationBasket createRegistrationBasket() {
+        LOG.info("Creating new registration basket");
+        return new RegistrationBasket();
+    }
 
+    @RequestMapping(value = "/register", name = "register", method = RequestMethod.GET)
+    public ModelAndView register(
+            @ModelAttribute("basket") RegistrationBasket basket) {
         return new ModelAndView(
                 "registration/register",
-                Map.of("categories", result.getData().membershipCategories())
+                Map.of("basket", basket)
         );
     }
 
-    @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/register/select-membership", name = "select-membership", method = RequestMethod.GET)
-    public ModelAndView selectMembership(Principal principal) throws IOException {
+    public ModelAndView selectMembership() throws IOException {
         MembershipCategoriesQuery query = new MembershipCategoriesQuery(StringFilter.builder().build());
         Response<MembershipCategoriesQuery.Data> result = membershipCategoryService.executeQuery(query);
 
