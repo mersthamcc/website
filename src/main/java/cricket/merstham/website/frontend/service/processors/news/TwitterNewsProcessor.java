1536b919a6de199ada62e73da37bc5ea0fc534fa
Chris Clayson <chris@clayson.org.uk>
2021-12-20 23:02:22 +0000

    WEB-5: Publish to Twitter

---
+++ src/main/java/cricket/merstham/website/frontend/service/processors/news/TwitterNewsProcessor.java
@@ -0,0 +1,67 @@
+package cricket.merstham.website.frontend.service.processors.news;
+
+import cricket.merstham.website.frontend.exception.EntitySaveException;
+import cricket.merstham.website.frontend.model.News;
+import cricket.merstham.website.frontend.service.TweetService;
+import cricket.merstham.website.frontend.service.processors.ItemProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import twitter4j.TwitterException;
+
+import java.util.List;
+
+import static org.apache.logging.log4j.util.Strings.isBlank;
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
+@Service("NewsTwitter")
+public class TwitterNewsProcessor implements ItemProcessor<News> {
+    public static final String TWEET_ID = "tweet_id";
+    private static Logger LOG = LoggerFactory.getLogger(TwitterNewsProcessor.class);
+
+    private final String baseUrl;
+    private final TweetService tweetService;
+
+    @Autowired
+    public TwitterNewsProcessor(@Value("${base-url}") String baseUrl, TweetService tweetService) {
+        this.baseUrl = baseUrl;
+        this.tweetService = tweetService;
+    }
+
+    @Override
+    public void postOpen(News item) {
+        item.setPublishToTwitter(hasTweet(item));
+    }
+
+    @Override
+    public void postSave(News item) {
+        try {
+            if (!item.isDraft()) {
+                LOG.info("Running Tweet processor on news item '{}'", item.getTitle());
+                if (item.isPublishToTwitter() && !hasTweet(item)) {
+                    var id = tweetService.tweet(
+                            isBlank(item.getSocialSummary()) ? item.getTitle() : item.getSocialSummary(),
+                            baseUrl + item.getLink().toString()
+                    );
+                    item.getAttributes().put(TWEET_ID, Long.toString(id));
+                } else if (!item.isPublishToTwitter()
+                        && hasTweet(item)) {
+                    tweetService.unTweet(Long.parseLong(item.getAttributes().get(TWEET_ID)));
+                    item.getAttributes().put(TWEET_ID, "");
+                }
+            } else if (hasTweet(item)) {
+                tweetService.unTweet(Long.parseLong(item.getAttributes().get(TWEET_ID)));
+                item.getAttributes().put(TWEET_ID, "");
+            }
+        } catch (TwitterException ex) {
+            throw new EntitySaveException("Error processing Tweet",
+                    List.of(ex.getMessage()));
+        }
+    }
+
+    private boolean hasTweet(News item) {
+        return item.getAttributes().containsKey(TWEET_ID) && isNotBlank(item.getAttributes().get(TWEET_ID));
+    }
+}
