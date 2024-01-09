d875e626eb37acd541f5aad2ef7ade266b39f608
Chris Clayson <chris@clayson.org.uk>
2024-01-09 00:51:00 +0000

    WEB-60: Add profiles to control session storage method

---
+++ frontend/src/main/java/cricket/merstham/website/frontend/configuration/RedisSessionConfiguration.java
@@ -0,0 +1,10 @@
+package cricket.merstham.website.frontend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+@Profile("redis-sessions")
+public class RedisSessionConfiguration {}
