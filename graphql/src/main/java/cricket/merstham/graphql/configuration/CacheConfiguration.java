c7137d069709a40d372ab23cc24aec6df5f07e12
Chris Clayson <chris@clayson.org.uk>
2024-01-09 19:51:30 +0000

    WEB-61: Add profiles to control cache storage method

--- graphql/src/main/java/cricket/merstham/graphql/configuration/CacheConfiguration.java
+++ graphql/src/main/java/cricket/merstham/graphql/configuration/CacheConfiguration.java
@@ -1,28 +1,12 @@
 package cricket.merstham.graphql.configuration;
 
-import cricket.merstham.graphql.cache.DynamoCacheConfiguration;
-import cricket.merstham.graphql.cache.DynamoCacheManager;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
-import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
-import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
-import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
-import software.amazon.awssdk.services.dynamodb.model.KeyType;
-import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
-import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
-import software.amazon.awssdk.services.dynamodb.model.TableClass;
-import software.amazon.awssdk.services.dynamodb.model.TimeToLiveSpecification;
-import software.amazon.awssdk.services.dynamodb.model.UpdateTimeToLiveRequest;
 
-import static java.util.Objects.nonNull;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
-// @EnableCaching
 public class CacheConfiguration {
 
     public static final String MEMBER_SUMMARY_CACHE = "member_summary";
@@ -51,72 +35,9 @@ public class CacheConfiguration {
     public static final String VENUES_FOR_MENU_CACHE = "venue_for_menu_cache";
     public static final String VENUE_ITEM_BY_ID_CACHE = "venue_item_by_id";
 
-    @Bean
-    public CacheManager cacheManager(
-            DynamoDbClient client, DynamoCacheConfiguration configuration) {
-        if (configuration.isCreateTable() && !tableExists(client, configuration)) {
-            createTable(client, configuration);
-        }
-
-        return new DynamoCacheManager(client, configuration);
-    }
-
-    @Bean
-    public DynamoDbClient dynamoDbClient(DynamoCacheConfiguration configuration) {
-        var builder =
-                DynamoDbClient.builder()
-                        .credentialsProvider(DefaultCredentialsProvider.builder().build())
-                        .region(Region.of(configuration.getRegion()));
-        if (nonNull(configuration.getEndpoint())) {
-            builder.endpointOverride(configuration.getEndpoint());
-        }
-        return builder.build();
-    }
-
-    private static void createTable(DynamoDbClient client, DynamoCacheConfiguration configuration) {
-        client.createTable(
-                CreateTableRequest.builder()
-                        .tableName(configuration.getTableName())
-                        .billingMode("PAY_PER_REQUEST")
-                        .tableClass(TableClass.STANDARD)
-                        .attributeDefinitions(
-                                AttributeDefinition.builder()
-                                        .attributeName(configuration.getCacheNameAttributeName())
-                                        .attributeType(ScalarAttributeType.S)
-                                        .build(),
-                                AttributeDefinition.builder()
-                                        .attributeName(configuration.getKeyAttributeName())
-                                        .attributeType(ScalarAttributeType.B)
-                                        .build())
-                        .keySchema(
-                                KeySchemaElement.builder()
-                                        .attributeName(configuration.getCacheNameAttributeName())
-                                        .keyType(KeyType.HASH)
-                                        .build(),
-                                KeySchemaElement.builder()
-                                        .attributeName(configuration.getKeyAttributeName())
-                                        .keyType(KeyType.RANGE)
-                                        .build())
-                        .build());
-        client.updateTimeToLive(
-                UpdateTimeToLiveRequest.builder()
-                        .tableName(configuration.getTableName())
-                        .timeToLiveSpecification(
-                                TimeToLiveSpecification.builder()
-                                        .attributeName(configuration.getTimeToLiveAttributeName())
-                                        .enabled(true)
-                                        .build())
-                        .build());
-    }
-
-    private static boolean tableExists(
-            DynamoDbClient client, DynamoCacheConfiguration configuration) {
-        try {
-            client.describeTable(
-                    DescribeTableRequest.builder().tableName(configuration.getTableName()).build());
-            return true;
-        } catch (ResourceNotFoundException ignored) {
-            return false;
-        }
+    public Set<String> getCacheNames() {
+        return Arrays.stream(this.getClass().getDeclaredFields())
+                .map(f -> f.getName())
+                .collect(Collectors.toSet());
     }
 }
