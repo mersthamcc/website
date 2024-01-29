c9e6cb58681e8d2a4ca0305a72ec90063c282b80
Chris Clayson <chris@clayson.org.uk>
2024-01-29 11:57:58 +0000

    WEB-67: Default Locale

--- frontend/src/main/java/cricket/merstham/website/frontend/FrontendApplication.java
+++ frontend/src/main/java/cricket/merstham/website/frontend/FrontendApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.util.Locale;
+
 @SpringBootApplication
 @EnableCaching
 public class FrontendApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.UK);
         SpringApplication.run(FrontendApplication.class, args);
     }
 }
--- graphql/src/main/java/cricket/merstham/graphql/GraphqlJavaApplication.java
+++ graphql/src/main/java/cricket/merstham/graphql/GraphqlJavaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Locale;
+
 @SpringBootApplication
 @EnableScheduling
 public class GraphqlJavaApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.UK);
         SpringApplication.run(GraphqlJavaApplication.class, args);
     }
 }
