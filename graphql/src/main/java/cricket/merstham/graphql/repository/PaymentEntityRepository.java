b33f22964e1580711458fca002762e66915401e5
Chris Clayson <chris@clayson.org.uk>
2024-02-18 23:59:01 +0000

    WEB-70: Add Sales Order Sync

--- graphql/src/main/java/cricket/merstham/graphql/repository/PaymentEntityRepository.java
+++ graphql/src/main/java/cricket/merstham/graphql/repository/PaymentEntityRepository.java
@@ -3,4 +3,9 @@ package cricket.merstham.graphql.repository;
 import cricket.merstham.graphql.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentEntityRepository extends JpaRepository<PaymentEntity, Integer> {}
+import java.util.List;
+
+public interface PaymentEntityRepository extends JpaRepository<PaymentEntity, Integer> {
+
+    List<PaymentEntity> findPaymentEntitiesByReconciledIsFalseAndCollectedIsTrue();
+}
