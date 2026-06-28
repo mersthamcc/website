205aee218c1fc01b3f49a095a05e91e00a5214b5
Chris Clayson <treasurer@mersthamcc.co.uk>
2026-06-28 17:26:03 +0100

    BAU: Add CORS header to Duck API

--- graphql/src/main/java/cricket/merstham/graphql/controllers/DisplaysController.java
+++ graphql/src/main/java/cricket/merstham/graphql/controllers/DisplaysController.java
@@ -3,6 +3,9 @@ package cricket.merstham.graphql.controllers;
 import cricket.merstham.graphql.services.DisplayService;
 import cricket.merstham.shared.dto.DuckStatistics;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +21,17 @@ public class DisplaysController {
     }
 
     @GetMapping("/api/duck-statistics/{year}")
-    public DuckStatistics getDuckStatsForYear(
+    public ResponseEntity<DuckStatistics> getDuckStatsForYear(
             @PathVariable int year, @Param("length") Integer length) {
         var limit = isNull(length) ? 10 : length;
-        return DuckStatistics.builder()
-                .allDuckStatistics(displayService.getAllDuckStatistics(year, limit))
-                .leagueDuckStatistics(displayService.getLeagueDuckStatistics(year, limit))
-                .build();
+        var headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        return new ResponseEntity<>(
+                DuckStatistics.builder()
+                        .allDuckStatistics(displayService.getAllDuckStatistics(year, limit))
+                        .leagueDuckStatistics(displayService.getLeagueDuckStatistics(year, limit))
+                        .build(),
+                headers,
+                HttpStatus.OK);
     }
 }
