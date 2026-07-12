d1284a1554ec455ab7677d87fde7e4ce89ec5e63
Chris Clayson <treasurer@mersthamcc.co.uk>
2026-07-13 00:52:11 +0100

    WEB-123: Add fixture matching

--- graphql/src/main/java/cricket/merstham/graphql/services/YouTubeService.java
+++ graphql/src/main/java/cricket/merstham/graphql/services/YouTubeService.java
@@ -4,10 +4,13 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import cricket.merstham.graphql.entity.FixtureEntity;
 import cricket.merstham.graphql.entity.LiveStreamEntity;
+import cricket.merstham.graphql.repository.FixtureRepository;
 import cricket.merstham.graphql.repository.LiveStreamRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +22,12 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
+import static cricket.merstham.graphql.services.PlayCricketService.HOME;
 import static java.util.Objects.isNull;
 
 @Service
@@ -33,17 +39,20 @@ public class YouTubeService {
     private final TokenService tokenService;
     private final LiveStreamRepository liveStreamRepository;
     private final String channelId;
+    private final FixtureRepository fixtureRepository;
 
     @Autowired
     public YouTubeService(
             @Value("${configuration.google.application-name}") String applicationName,
             TokenService tokenService,
             LiveStreamRepository liveStreamRepository,
-            @Value("${configuration.live-stream.youtube-channel-id}") String channelId) {
+            @Value("${configuration.live-stream.youtube-channel-id}") String channelId,
+            FixtureRepository fixtureRepository) {
         this.tokenService = tokenService;
         this.applicationName = applicationName;
         this.liveStreamRepository = liveStreamRepository;
         this.channelId = channelId;
+        this.fixtureRepository = fixtureRepository;
     }
 
     @Scheduled(
@@ -116,6 +125,7 @@ public class YouTubeService {
                                                             video.getLiveStreamingDetails()
                                                                     .getScheduledEndTime()))
                                             .setFrogboxId(frogboxId.get())
+                                            .setFixture(matchFixture(video))
                                             .setThumbnailUrl(
                                                     video.getSnippet()
                                                             .getThumbnails()
@@ -132,6 +142,24 @@ public class YouTubeService {
             LOG.error("Error getting YouTube data", e);
         }
         LOG.info("*** End of live stream sync ***");
+    }
+
+    private FixtureEntity matchFixture(Video video) {
+        var start = convertToInstant(video.getLiveStreamingDetails().getScheduledStartTime());
+        var title = video.getSnippet().getTitle();
+
+        var fixture =
+                fixtureRepository
+                        .findAllByDateIn(
+                                List.of(LocalDate.ofInstant(start, ZoneId.systemDefault())))
+                        .stream()
+                        .filter(
+                                f ->
+                                        title.contains(f.getTeam().getName())
+                                                && title.contains(f.getOpposition())
+                                                && f.getHomeAway().equals(HOME))
+                        .findFirst();
+        return fixture.orElse(null);
     }
 
     private Optional<String> getFrogboxTag(List<String> tags) {
